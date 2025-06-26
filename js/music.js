486986df356a878ba182e4c81dee160a22c8575e
MRJ <MRJ@LAPTOP-49JL3G8C.(none)>
2025-06-26 23:35:52 +0800

    Site updated: 2025-06-26 23:35:52

--- js/music.js
+++ js/music.js
@@ -8,7 +8,6 @@ const ap = new APlayer({
         artist: '邓紫棋',
         url: 'https://i.y.qq.com/n2/m/outchain/player/index.html?songid=104883226&songtype=0',
         cover: 'https://y.qq.com/music/photo_new/T002R300x300M000003c616O2Zlswm_2.jpg?max_age=2592000',	
-    },
+    }
 	]
 });
-
